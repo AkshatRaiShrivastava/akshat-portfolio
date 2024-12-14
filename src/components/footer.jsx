f6a86bbb484d4c221d6a56cf4e46e73a303a8823
AkshatRaiShrivastava <shrivastavak2005@gmail.com>
2024-12-14 15:32:32 +0530

    footer text size

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,8 +5,8 @@ const Footer=()=>{
         <>
         <div className="relative bottom-0 flex flex-row mb-10 items-center justify-between lg:px-10">
 
-        <img src={logo}width={70} />
-        <h3 className="tracking-wider text-neutral-400">This website is open source on <a className="text-neutral-200" href="https://github.com/AkshatRaiShrivastava/akshat-portfolio">GitHub</a></h3>
+        <img className="md:w-50" src={logo}width={70} />
+        <h3 className="md:text-sm tracking-wider text-neutral-400">This website is open source on <a className="text-neutral-200" href="https://github.com/AkshatRaiShrivastava/akshat-portfolio">GitHub</a></h3>
         </div>
         </>
     );
